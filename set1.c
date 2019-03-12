60bb73e0a3bd043540cc844598d5d490d1ae3b27
klHaritha <48052673+klHaritha@users.noreply.github.com>
2019-03-12 13:29:58 +0530

    Update set1.c

--- set1.c
+++ set1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+void main()
+{
+    int a;
+    printf("enter num:");
+    scanf("%d",&a);
+    if(a%2==0)
+     {
+        printf("even");
+        
+      }
+    else
+    {
+        printf("invalid");
+      }
+}
